eaa68b54033fbc55481ef4b6f00c0edead1c745a
Eckart Cobo Briesewitz <eck4art@gmail.com>
2025-10-19 10:54:55 +0200

    added wrist camera and pnada single scene

--- experiments/panda/control.py
+++ experiments/panda/control.py
@@ -1,9 +1,10 @@
 import time
 import mujoco
 import numpy as np
+import matplotlib.pyplot as plt
 from explore.env.mujoco_sim import MjSim
 
-sim = MjSim("configs/franka_emika_panda/scene.xml", 0.001, view=True, verbose=0)
+sim = MjSim("configs/franka_emika_panda/panda_single.xml", 0.001, view=True, verbose=0)
 
 ctrl_joint_ids = [
     sim.model.jnt_qposadr[sim.model.actuator_trnid[i][0]]
@@ -22,13 +23,16 @@ tau_action = 10
 ctrl_time = 0.0
 q_target = np.array([
     0, 0, 0, -1.57079, 0, 1.57079, -0.7853, 255,
-    0, 0, 0, -1.57079, 0, 1.57079, -0.7853, 255
+    # 0, 0, 0, -1.57079, 0, 1.57079, -0.7853, 255
 ])
 config = np.array([
     0, 0, 0, -1.57079, 0, 1.57079, -0.7853, 0.04, 0.04,
     0, 0, 0, -1.57079, 0, 1.57079, -0.7853, 0.04, 0.04,
     0, 0, 0.7, 1, 0, 0, 0
 ])
-sim.pushConfig(config)
+# sim.pushConfig(config)
 time.sleep(5)
-sim.step(tau_action, view=tau_action)
+sim.step(tau_action, ctrl_target=q_target, view=tau_action)
+sim.setupRenderer(camera="l_wrist_cam")
+img = sim.renderImg()
+plt.imsave("experiments/wrist_img.png", img)
