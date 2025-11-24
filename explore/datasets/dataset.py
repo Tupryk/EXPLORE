data normalization and cuda memory fix

explore/datasets/dataset.py
@@ -53,6 +53,7 @@ class ExploreDataset(Dataset):
                 
             path_states = torch.tensor(path_states, dtype=torch.float).unsqueeze(1)
             path_actions = torch.tensor(path_actions, dtype=torch.float).unsqueeze(1)
+
             self.states.append(path_states)
             self.actions.append(path_actions)
             
@@ -61,9 +62,6 @@ class ExploreDataset(Dataset):
             self.episode_idxs.extend([i for _ in range(traj_len)])
             goal = torch.tensor(self.trees[end_idx][0]["state"][1], dtype=torch.float) * self.q_mask
             self.goal_states.append(goal)
-            
-        self.actions = torch.tensor(self.actions)
-        self.states = torch.tensor(self.states)
             
         # TODO: Change tau_action
             
@@ -75,8 +73,10 @@ class ExploreDataset(Dataset):
             print(f"State shape: {self.states[0][0].shape[1]}")
         
         print("Normalizing dataset...")
-        self.action_normalizer = MinMaxNormalizer(self.actions[:, 0, :])
-        self.state_normalizer = MinMaxNormalizer(self.states[:, 0, :])
+        min_max_states  = torch.cat(self.states, dim=0)
+        min_max_actions = torch.cat(self.actions, dim=0)
+        self.action_normalizer = MinMaxNormalizer(min_max_actions[:, 0, :])
+        self.state_normalizer = MinMaxNormalizer(min_max_states[:, 0, :])
         
         assert len(self.episode_idxs) == sum(self.episode_lengths)
 
@@ -107,8 +107,6 @@ class ExploreDataset(Dataset):
                 idx = -1
             action = torch.cat((action, episode_actions[idx]), dim=0)
 
-        action = self.action_normalizer.normalize(action)
-        state = self.state_normalizer.normalize(state)
         return action, state, self.goal_states[episode_idx]
 
     def play_episode(self, idx: int):

explore/env/utils.py
@@ -6,70 +6,75 @@ import gymnasium as gym
 import matplotlib.pyplot as plt
 
 
-def eval_il_policy(policy : nn.Module, env: gym.Env, history: int=1, horizon: int=1,
-                   save_path: str="", eval_count: int=1) -> list[float]:
+def eval_il_policy(
+    policy : nn.Module, env: gym.Env,
+    history: int=1,
+    save_path: str="",
+    eval_count: int=1,
+    ) -> list[float]:
 
-    policy.eval()
-    all_rewards = []
-    for i in range(eval_count):
-        
-        options = {"eval_view": "fixed_cam"}
-        new_obs, init_info = env.reset(options=options)
-
-        imgs = []
-        done = False
-        obs = torch.from_numpy(new_obs)
-        obs = obs.expand(1, history, obs.shape[0])
-        
-        episode_rewards = []
-        while not done:
+    with torch.no_grad():
+        policy.eval()
+        all_rewards = []
+        for i in range(eval_count):
             
-            new_obs = torch.from_numpy(new_obs)
-            history = obs.shape[1]
-            shifted = obs.clone()
-            shifted[:, :history-1, :] = obs[:, 1:, :]
-            shifted[:, -1, :] = new_obs
-            obs = shifted.float()
+            options = {"eval_view": "fixed_cam"}
+            new_obs, init_info = env.reset(options=options)
 
-            obs_in = obs[:, :, :-policy.cond_dim].clone()
-            goal_cond = obs[:, 0, -policy.cond_dim:].clone()
-
-            actions = policy(obs_in.to(policy.device), goal_cond.to(policy.device))["pred"]
-            actions = actions.detach().cpu().numpy()[0]
+            imgs = []
+            done = False
+            obs = torch.from_numpy(new_obs)
+            obs = obs.expand(1, history, obs.shape[0])
             
-            for a in actions:
-                new_obs, reward, terminated, truncated, info = env.step(a)
+            episode_rewards = []
+            while not done:
                 
-                frames = info["frames"]
-                imgs.extend(frames)
-                done = terminated or truncated
-                episode_rewards.append(reward)
+                new_obs = torch.from_numpy(new_obs)
+                history = obs.shape[1]
+                shifted = obs.clone()
+                shifted[:, :history-1, :] = obs[:, 1:, :]
+                shifted[:, -1, :] = new_obs
+                obs = shifted.float()
+
+                obs_in = obs[:, :, :-policy.cond_dim].clone()
+                goal_cond = obs[:, 0, -policy.cond_dim:].clone()
+
+                actions = policy(obs_in.to(policy.device), goal_cond.to(policy.device))["pred"]
+                actions = actions.detach().cpu().numpy()[0]
                 
-                print(f"Iter: {env.iter}; Reward: {env.reward:.4f} (Goal Reward: {info['goal_reward']:.4f} Guiding Reward: {info['guiding_reward']:.4f})")
-        
-        all_rewards.append(episode_rewards)
+                for a in actions:
+                    new_obs, reward, terminated, truncated, info = env.step(a)
+                    
+                    frames = info["frames"]
+                    imgs.extend(frames)
+                    done = terminated or truncated
+                    episode_rewards.append(reward)
+                    
+                    print(f"Iter: {env.iter}; Reward: {env.reward:.4f} (Goal Reward: {info['goal_reward']:.4f} Guiding Reward: {info['guiding_reward']:.4f})")
+            
+            all_rewards.append(episode_rewards)
 
-        if save_path:
-            result_path = os.path.join(save_path, f"result{i}.gif")
-            imageio.mimsave(result_path, imgs, fps=24, loop=0)
+            if save_path:
+                result_path = os.path.join(save_path, f"result{i}.gif")
+                imageio.mimsave(result_path, imgs, fps=24, loop=0)
 
-            im_start = env.render("fixed_cam", config_idx=init_info["start_config_idx"])
-            im_end = env.render("fixed_cam", config_idx=init_info["end_config_idx"])
-            im_reached = env.render("fixed_cam")
+                im_start = env.render("fixed_cam", config_idx=init_info["start_config_idx"])
+                im_end = env.render("fixed_cam", config_idx=init_info["end_config_idx"])
+                im_reached = env.render("fixed_cam")
 
-            fig, axes = plt.subplots(1, 3, figsize=(30, 20))
-            axes[0].set_title("Start Config", fontsize=24, fontweight="bold")
-            axes[0].imshow(im_start)
-            axes[0].axis("off")
-            axes[1].set_title("Target Config", fontsize=24, fontweight="bold")
-            axes[1].imshow(im_end)
-            axes[1].axis("off")
-            axes[2].set_title("Reached Config", fontsize=24, fontweight="bold")
-            axes[2].imshow(im_reached)
-            axes[2].axis("off")
-            plt.tight_layout()
-            plt.savefig(os.path.join(save_path, f"intro{i}.png"))
-            plt.close('all')
+                fig, axes = plt.subplots(1, 3, figsize=(30, 20))
+                axes[0].set_title("Start Config", fontsize=24, fontweight="bold")
+                axes[0].imshow(im_start)
+                axes[0].axis("off")
+                axes[1].set_title("Target Config", fontsize=24, fontweight="bold")
+                axes[1].imshow(im_end)
+                axes[1].axis("off")
+                axes[2].set_title("Reached Config", fontsize=24, fontweight="bold")
+                axes[2].imshow(im_reached)
+                axes[2].axis("off")
+                plt.tight_layout()
+                plt.savefig(os.path.join(save_path, f"intro{i}.png"))
+                plt.close('all')
 
     return all_rewards
     

explore/train/il_trainer.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 from explore.env.utils import eval_il_policy
+from explore.train.utils import warmup_cos_scheduler
 
 
 class IL_Trainer:
@@ -35,10 +36,11 @@ class IL_Trainer:
         self.checkpoint_path = os.path.join(cfg.output_dir, "checkpoints")
         os.makedirs(os.path.dirname(self.checkpoint_path), exist_ok=True)
 
-    def train(self, epochs: int = 10, log_interval: int = 100, env: gym.Env=None):
+    def train(self, epochs, log_interval: int=100, env: gym.Env=None):
         
-        global_step = 0
+        scheduler = warmup_cos_scheduler(self.optimizer, epochs, epochs//100)
 
+        global_step = 0
         for epoch in range(1, epochs + 1):
             self.policy.train()
             epoch_loss = 0.0
@@ -52,6 +54,7 @@ class IL_Trainer:
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+                scheduler.step()
 
                 epoch_loss += loss.item()
                 if batch_idx % log_interval == 0:
@@ -75,9 +78,13 @@ class IL_Trainer:
                 env_evals_path = os.path.join(ckpt_path, "env_evals")
                 os.makedirs(env_evals_path, exist_ok=True)
                 if env != None:
-                    eval_il_policy(self.policy, env, save_path=env_evals_path,
-                                   eval_count=self.sim_eval_count, history=self.policy.history, horizon=self.policy.horizon)
-
+                    eval_il_policy(
+                        self.policy, env,
+                        save_path=env_evals_path,
+                        eval_count=self.sim_eval_count,
+                        history=self.policy.history
+                    )
+            
         self.writer.close()
         print(f"Training complete. Model saved to {self.checkpoint_path}")
         